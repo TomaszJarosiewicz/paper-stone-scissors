33ec7ff98bceebabdf74f783464fce3177fa04e9
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-07-22 22:53:07 +0200

    Add function for script.js and modern index.html

--- js/script.js
+++ js/script.js
@@ -14,6 +14,12 @@ const items = [
 ];
 
 const allItems = Object.assign(items);
+const win = document.querySelector('.game-over');
+const winPlayer = document.querySelector('.win-player');
+const text = document.querySelector('.text');
+const button = document.querySelector('button');
+const round = document.querySelector('#round');
+const player = document.querySelector('.player');
 const playPaper = document.querySelector('#playPaper');
 const playStone = document.querySelector('#playStone');
 const playScissors = document.querySelector('#playScissors');
@@ -22,12 +28,22 @@ const showComputerItem = document.querySelector('.showComputerItem');
 const game = document.querySelector('.game');
 const playerCount = document.querySelector('.player-count');
 const computerCount = document.querySelector('.computer-count');
+let playerName;
 let chooseItem;
+let counter = 0;
 
 playerCount.innerHTML = 0;
 computerCount.innerHTML = 0;
+round.innerHTML = 1;
 
 const playGame = (() => {
+
+    const writeNamePlayer = () => {
+        playerName = prompt('Your name');
+        if(playGame != null) {
+            player.innerText = playerName;
+        }
+    }
 
     const randomItem = () => {
         chooseItem = allItems[Math.floor(Math.random() * allItems.length)];
@@ -37,40 +53,46 @@ const playGame = (() => {
             return chooseItem.id;
         }
 
-    // const addClassForWinner = (element) => {
-    //     return element.classList.add('winner');
-    // }
-    // const removeClassForWinner = (element) => {
-    //     return element.classList.add('winner');
-    // }
+    const addClassForWinner = (elementAdd, elementRemove) => {
+        elementAdd.classList.add('winner');
+        elementRemove.classList.remove('winner');
+    }
 
     const displayItemsPlayer = () => {
         allItems.forEach(el => {
             if(el.id === 1) {
                 playPaper.addEventListener('click', () => {
+                    round.innerHTML = counter++ + 1;
                     const paper = el.item;
                     showPlayerItem.innerHTML = paper;
                     showComputerItem.innerHTML = randomItem();
                     switch(randomItem()) {
                         case 1: {
                             console.log('Draw');
+                            showPlayerItem.classList.remove('winner');
+                            showComputerItem.classList.remove('winner');
                         break;
                         }
                         case 2: {
                             console.log('Player wins');
                             playerCount.innerHTML = 1 + parseInt(playerCount.innerHTML);
+                            addClassForWinner(showPlayerItem, showComputerItem);
                         break;
                         }
                         case 3: {
                             console.log('Computer wins');
                             computerCount.innerHTML = 1 + parseInt(computerCount.innerHTML);
+                            addClassForWinner(showComputerItem, showPlayerItem);
                         break;
                         }
                     }
+                    gameOver();
                 });
             }
+
             if(el.id === 2) {
                 playStone.addEventListener('click', () => {
+                    round.innerHTML = counter++ + 1;
                     const stone = el.item;
                     showPlayerItem.innerHTML = stone;
                     showComputerItem.innerHTML = randomItem();
@@ -78,22 +100,29 @@ const playGame = (() => {
                         case 1: {
                             console.log('Computer Wins');
                             computerCount.innerHTML = 1 + parseInt(computerCount.innerHTML);
+                            addClassForWinner(showComputerItem, showPlayerItem);
                         break;
                         }
                         case 2: {
                             console.log('Draw');
+                            showPlayerItem.classList.remove('winner');
+                            showComputerItem.classList.remove('winner');
                         break;
                         }
                         case 3: {
                             console.log('Player wins');
                             playerCount.innerHTML = 1 + parseInt(playerCount.innerHTML);
+                            addClassForWinner(showPlayerItem, showComputerItem);
                         break;
                         }
                     }
+                    gameOver();
                 });
             }
+
             if(el.id === 3) {
                 playScissors.addEventListener('click', () => {
+                    round.innerHTML = counter++ + 1;
                     const scissors = el.item;
                     showPlayerItem.innerHTML = scissors;
                     showComputerItem.innerHTML = randomItem();
@@ -101,26 +130,56 @@ const playGame = (() => {
                         case 1: {
                             console.log('Player Wins');
                             playerCount.innerHTML = 1 + parseInt(playerCount.innerHTML);
+                            addClassForWinner(showPlayerItem, showComputerItem);
                         break;
                         }
                         case 2: {
                             console.log('Computer Wins');
                             computerCount.innerHTML = 1 + parseInt(computerCount.innerHTML);
+                            addClassForWinner(showComputerItem, showPlayerItem);
                         break;
                         }
                         case 3: {
                             console.log('Draw');
+                            showPlayerItem.classList.remove('winner');
+                            showComputerItem.classList.remove('winner');
                         break;
                         }
                     }
+                    gameOver();
                 });
             }
+        });
+    }
+
+    const gameOver = () => {
+        if(counter > 20) {
+            round.innerHTML = 20;
+            win.style.display = 'block';
+            if(playerCount.innerHTML > computerCount.innerHTML) {
+                text.innerHTML = 'the winner <br /> is';
+                winPlayer.innerHTML = playerName;
+            } else if(playerCount.innerHTML === computerCount.innerHTML) {
+                text.innerHTML = 'game finished';
+                winPlayer.innerHTML = 'draw';
+            } else {
+                text.innerHTML = 'the winner <br /> is';
+                winPlayer.innerHTML = 'computer';
+            }
+        }
+    }
+
+    const newGame = () => {
+        button.addEventListener('click', () => {
+            location.reload();
         });
     }
 
     return {
         showReturnItems: () => {
             displayItemsPlayer();
+            writeNamePlayer();
+            newGame();
         }
     }
 
