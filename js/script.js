85e273e8b2cfc6334fcef10f31616e4217916f5f
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-07-26 21:39:35 +0200

    Fixbug script.js

--- js/script.js
+++ js/script.js
@@ -14,13 +14,18 @@ const items = [
 ];
 
 const allItems = Object.assign(items);
+const yourName = document.querySelector('.yourName');
+const container = document.querySelector('.container');
+const playerItems = document.querySelectorAll('.flex > .item');
 const player = document.querySelector('.player');
+const playerName = document.querySelector('#playerName');
+const showBtn = document.querySelector('#showBtn');
 const playPaper = document.querySelector('#playPaper');
 const playStone = document.querySelector('#playStone');
 const playScissors = document.querySelector('#playScissors');
 const showPlayerItem = document.querySelector('.showPlayerItem');
 const showComputerItem = document.querySelector('.showComputerItem');
-const game = document.querySelector('.game');
+const resultGame = document.querySelector('#resultGame');
 const round = document.querySelector('#round');
 const playerCount = document.querySelector('.player-count');
 const computerCount = document.querySelector('.computer-count');
@@ -30,30 +35,50 @@ const winPlayer = document.querySelector('.win-player');
 const text = document.querySelector('.text');
 const button = document.querySelector('button');
 
-let playerName;
 let chooseItem;
 let counter = 0;
 
 playerCount.innerHTML = 0;
 computerCount.innerHTML = 0;
 round.innerHTML = 1;
+container.style.display = 'none';
 
 const playGame = (() => {
 
     const writeNamePlayer = () => {
-        playerName = prompt('Your name');
-        if(playGame != null) {
-            player.innerText = playerName;
+        showBtn.addEventListener('click', () => {
+            if(playerName.value.length === 0) {
+                playerName.style.border = '1px solid #cc0c0c';
+                container.style.display = 'none';
+            } else {
+                yourName.style.display = 'none';
+                container.style.display = 'block';
+                player.innerHTML = playerName.value;
+            }
+        });
+    }
+
+    const displayItemsPlayer = () => {
+        for(let i = 0; i < playerItems.length; i++) {
+            allItems.forEach(index => {
+                if(index.id === 1) {
+                    playerItems[0].innerHTML = index.item;
+                }
+                if(index.id === 2) {
+                    playerItems[1].innerHTML = index.item;
+                }
+                if(index.id === 3) {
+                    playerItems[2].innerHTML = index.item;
+                }
+            });
         }
     }
 
     const randomItem = () => {
         chooseItem = allItems[Math.floor(Math.random() * allItems.length)];
-            [chooseItem].forEach(item => {
-                showComputerItem.innerHTML = item.item;
-            });
-            return chooseItem.id;
-        }
+            [chooseItem].forEach(item => { showComputerItem.innerHTML = item.item });
+        return chooseItem.id;
+    }
 
     const addClassForWinner = (elementAdd, elementRemove) => {
         elementAdd.classList.add('winner');
@@ -70,19 +95,19 @@ const playGame = (() => {
                     showComputerItem.innerHTML = randomItem();
                     switch(randomItem()) {
                         case 1: {
-                            console.log('Draw');
+                            resultGame.innerHTML = 'draw';
                             showPlayerItem.classList.remove('winner');
                             showComputerItem.classList.remove('winner');
                         break;
                         }
                         case 2: {
-                            console.log('Player wins');
+                            resultGame.innerHTML = playerName.value + '&nbsp;wins';
                             playerCount.innerHTML = 1 + parseInt(playerCount.innerHTML);
                             addClassForWinner(showPlayerItem, showComputerItem);
                         break;
                         }
                         case 3: {
-                            console.log('Computer wins');
+                            resultGame.innerHTML = 'computer wins';
                             computerCount.innerHTML = 1 + parseInt(computerCount.innerHTML);
                             addClassForWinner(showComputerItem, showPlayerItem);
                         break;
@@ -100,19 +125,19 @@ const playGame = (() => {
                     showComputerItem.innerHTML = randomItem();
                     switch(randomItem()) {
                         case 1: {
-                            console.log('Computer Wins');
+                            resultGame.innerHTML = 'computer wins';
                             computerCount.innerHTML = 1 + parseInt(computerCount.innerHTML);
                             addClassForWinner(showComputerItem, showPlayerItem);
                         break;
                         }
                         case 2: {
-                            console.log('Draw');
+                            resultGame.innerHTML = 'draw';
                             showPlayerItem.classList.remove('winner');
                             showComputerItem.classList.remove('winner');
                         break;
                         }
                         case 3: {
-                            console.log('Player wins');
+                            resultGame.innerHTML = playerName.value + '&nbsp;wins';
                             playerCount.innerHTML = 1 + parseInt(playerCount.innerHTML);
                             addClassForWinner(showPlayerItem, showComputerItem);
                         break;
@@ -130,19 +155,19 @@ const playGame = (() => {
                     showComputerItem.innerHTML = randomItem();
                     switch(randomItem()) {
                         case 1: {
-                            console.log('Player Wins');
+                            resultGame.innerHTML = playerName.value + '&nbsp;wins';
                             playerCount.innerHTML = 1 + parseInt(playerCount.innerHTML);
                             addClassForWinner(showPlayerItem, showComputerItem);
                         break;
                         }
                         case 2: {
-                            console.log('Computer Wins');
+                            resultGame.innerHTML = 'computer wins';
                             computerCount.innerHTML = 1 + parseInt(computerCount.innerHTML);
                             addClassForWinner(showComputerItem, showPlayerItem);
                         break;
                         }
                         case 3: {
-                            console.log('Draw');
+                            resultGame.innerHTML = 'draw';
                             showPlayerItem.classList.remove('winner');
                             showComputerItem.classList.remove('winner');
                         break;
@@ -155,13 +180,13 @@ const playGame = (() => {
     }
 
     const gameOver = () => {
-        const theWinner = 'the winner <br /> is';
+        const theWinner = 'the winner<br />is';
         if(counter > 20) {
             round.innerHTML = 20;
             win.style.display = 'block';
             if(playerCount.innerHTML > computerCount.innerHTML) {
                 text.innerHTML = theWinner;
-                winPlayer.innerHTML = playerName;
+                winPlayer.innerHTML = playerName.value;
             } else if(playerCount.innerHTML === computerCount.innerHTML) {
                 text.innerHTML = 'game finished';
                 winPlayer.innerHTML = 'draw';
@@ -180,6 +205,7 @@ const playGame = (() => {
 
     return {
         returnGame: () => {
+            displayItemsPlayer();
             displayItems();
             writeNamePlayer();
             newGame();
